4f2fdaadd5700c93efc4a36d0f30e87b733004f2
Inès Damiche <damiche.ines@gmail.com>
2026-03-17 15:28:32 +0100

    Add staking contract

---
+++ Staking.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Staking {
+
+    address public owner;
+    uint public minimumStake = 1 ether;
+
+    mapping(address => uint) public stakes;
+
+    event Staked(address user, uint amount);
+    event Released(address user, uint amount);
+    event Slashed(address user);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    constructor() payable {
+        owner = msg.sender;
+        require(msg.value >= minimumStake, "Minimum funding required");
+    }
+
+    function stake() public payable {
+        require(msg.value >= minimumStake, "Stake too small");
+
+        stakes[msg.sender] += msg.value;
+        emit Staked(msg.sender, msg.value);
+    }
+
+    function releaseStake(address proposer) public onlyOwner {
+
+        uint amount = stakes[proposer];
+        require(amount > 0, "No stake found");
+
+        stakes[proposer] = 0;
+        payable(proposer).transfer(amount);
+
+        emit Released(proposer, amount);
+    }
+
+    function slashStake(address proposer) public onlyOwner {
+        stakes[proposer] = 0;
+        emit Slashed(proposer);
+    }
+}
