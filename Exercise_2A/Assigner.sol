b6e6bee441125fc4c70bc3af2c3dceb8324f3a6a
pasdideepourunnom <delattre.maudlucie@gmail.com>
2026-03-18 10:56:53 +0100

    Assigner changes

--- Exercise_2A/Assigner.sol
+++ Exercise_2A/Assigner.sol
@@ -1,34 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// PAS ENCORE TESTE
-// attente du pull pour les autres documents
-// import "./node.sol";
+import "node.sol";
 
 contract Assigner {
     address[] public nodes;
 
     // register new node
     function registerNode(address nodeAddress) external {
+        require(nodeAddress != address(0), "Invalid node address");
         nodes.push(nodeAddress);
     }
 
     // update peers to maintain the circle
     function assignPeer(address nodeAddress) external {
+        require(nodes.length > 0, "No nodes registered");
         uint total = nodes.length;
+
         if (total == 1) {
-            // a single node should point to itself.
-            Node(nodeAddress).set_peer(nodeAddress);
+            Node(nodeAddress).add_peer(nodeAddress);
         } else {
             address head = nodes[0];
             address newTail = nodes[total - 1];
             address oldTail = nodes[total - 2];
 
-            // update the old end of the line to point to the new person
-            Node(oldTail).set_peer(newTail);
-            
-            // close the ring
-            Node(newTail).set_peer(head);
+            Node(oldTail).add_peer(newTail);
+            Node(newTail).add_peer(head);
         }
     }
 }
