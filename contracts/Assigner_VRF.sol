416980f840b54579e880fd674b23c7bf75053071
pasdideepourunnom <delattre.maudlucie@gmail.com>
2026-03-21 14:24:06 +0100

    Assigner VRF final

--- contracts/Assigner_VRF.sol
+++ contracts/Assigner_VRF.sol
@@ -1,47 +1,70 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// select a random leader from a list of nodes
+import "./node.sol";
 
+// VRF imports
 // talking to the oracle 
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
-// Verifiable random function infrastructure 
-contract AssignerVRF is VRFConsumerBaseV2Plus {
-    
-    // nodes in the ring
-    address[] public nodes;
+contract Assigner is VRFConsumerBaseV2Plus {
 
-    // variables to make a request
+    // Ring network
+
+    address[] public nodes;
+    address public currentProposer;
+
+    // VARIABLES TO MAKE A REQUEST
+
     uint256 public s_subscriptionId; 
     bytes32 public s_keyHash; 
     uint32 public s_callbackGasLimit = 100000;
     uint16 public s_requestConfirmations = 3;
     uint32 public s_numWords = 1; 
+    uint256[] public s_randomWords;
     uint256 public s_requestId;
 
-    address public currentProposer;
+    event ReturnedRandomness(uint256[] randomWords);
 
-   constructor(address vrfCoordinator) VRFConsumerBaseV2Plus(vrfCoordinator) {}
+    // Constructor
+    constructor(
+        uint256 subscriptionId,
+        address vrfCoordinator,
+        bytes32 keyHash
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
+        s_keyHash = keyHash;
+        s_subscriptionId = subscriptionId;
+    }
 
-   function requestRandomWords() external onlyOwner {
+    // VRF request
+
+    function requestRandomWords() external onlyOwner {
     // Will revert if subscription is not set and funded.
-    s_requestId = s_vrfCoordinator.requestRandomWords(
-      VRFV2PlusClient.RandomWordsRequest({
-        keyHash: s_keyHash,
-        subId: s_subscriptionId,
-        requestConfirmations: REQUEST_CONFIRMATIONS,
-        callbackGasLimit: CALLBACK_GAS_LIMIT,
-        numWords: NUM_WORDS,
-        extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: false}))
-      })
-    );
-  }
+        s_requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: s_keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: s_requestConfirmations,
+                callbackGasLimit: s_callbackGasLimit,
+                numWords: s_numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
+    }
 
-// This is the function the Oracle calls back with the answer
-    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {
-        // : Use the random number to pick a proposer
+    // VRF response
+    // This is the function the Oracle calls back with the answer
+
+    function fulfillRandomWords(uint256, uint256[] calldata randomWords) internal override {
+        s_randomWords = randomWords;
+        emit ReturnedRandomness(randomWords);
+
+        // select proposer
+        require(nodes.length > 0, "No nodes available");
+
         // we use '%' (modulo) to get a number between 0 and the list size
         uint256 index = randomWords[0] % nodes.length;
 
@@ -49,9 +72,34 @@ contract AssignerVRF is VRFConsumerBaseV2Plus {
         currentProposer = nodes[index];
     }
 
-    // function to add nodes to the list
-    function registerNode(address _node) public {
-        nodes.push(_node);
+
+    // register new node
+    function registerNode(address nodeAddress) external {
+        require(nodeAddress != address(0), "Invalid node address");
+        nodes.push(nodeAddress);
     }
 
+    // update peers to maintain the circle
+
+    function assignPeer(address nodeAddress) external {
+        require(nodes.length > 0, "No nodes available");
+
+        uint total = nodes.length;
+
+        if (total == 1) {
+            Node(nodeAddress).add_peer(nodeAddress);
+        } else {
+            address head = nodes[0];
+            address newTail = nodes[total - 1];
+            address oldTail = nodes[total - 2];
+
+            Node(oldTail).add_peer(newTail);
+            Node(newTail).add_peer(head);
+        }
+    }
+
+
+    function getCurrentProposer() public view returns (address) {
+        return currentProposer;
+    }
 }
